14cf2217a94b0559d233f8e6efe9713a841939d4
saurabhdhingra <saurabh20work@gmail.com>
2026-01-07 14:08:35 +0530

    Slack + Storage Factory

--- internal/notify/slack.go
+++ internal/notify/slack.go
@@ -1,1 +1,41 @@
 package notify
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type SlackPayload struct {
+	Text string `json:"text"`
+}
+
+func SendSlackNotification(webhookURL, message string) error {
+	fmt.Printf("DEBUG: Sending to %s...\n", webhookURL)
+	payload := SlackPayload{Text: message}
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("DEBUG: Payload: %s\n", string(data))
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	resp, err := client.Post(webhookURL, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	fmt.Printf("DEBUG: Slack Response Code: %d\n", resp.StatusCode)
+	fmt.Printf("DEBUG: Slack Response Body: %s\n", buf.String())
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("slack notification failed: %s", buf.String())
+	}
+
+	return nil
+}
--- internal/storage/factory.go
+++ internal/storage/factory.go
@@ -1,1 +1,16 @@
 package storage
+
+import (
+	"fmt"
+)
+
+func NewStorage(cfg Config) (Storage, error) {
+	switch cfg.Type {
+	case "local":
+		return NewLocal(cfg), nil
+	case "s3", "aws":
+		return NewS3(cfg)
+	default:
+		return nil, fmt.Errorf("unsupported storage type: %s", cfg.Type)
+	}
+}
